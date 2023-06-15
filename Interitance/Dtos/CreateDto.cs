9d285ef78987e3c8addf7b1c8d7887df2d51e299
cuong.chi.tran <cuong.chi.tran@topicus.nl>
2023-06-15 17:05:01 +0700

    find the way to solve update converter

---
+++ Interitance/Dtos/CreateDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Interitance.Dtos
+{
+    public class CreateDto : AnimalDto
+    {
+        public string Cat { get; set; }
+
+        public string Dog { get; set; }
+
+        public string Type { get; set; }
+    }
+}
